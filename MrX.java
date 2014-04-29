e6d621ee75fe38bf34781558b5d784b2cd43de82
Steven Janovitz <stevenjanovitz@wustl.edu>
2014-04-29 18:21:44 -0500

    Removed Player member variables from derived classes

--- MrX.java
+++ MrX.java
@@ -1,11 +1,6 @@
 import java.util.*;
 
 public class MrX extends Player {
-	private String name;
-	private int id;
-	private int[] tickets;
-	private Node location;
-	private LinkedList<Node> moveLog;
 	
 	public MrX(String name, int id){
 		super(name, id);
