99fe2c4fecfbd4342e0f0a3fc40043ab6afc6103
Steven Janovitz <stevenjanovitz@wustl.edu>
2014-06-19 23:18:59 -0400

    Initial Unity project: added map scrolling, switching between main menu and game, moving the detective object by selecting and touching another point, and a persistent UI overlay in the game

---
+++ unity/Assets/GameplayGUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameplayGUI : MonoBehaviour {
+
+	private int btnWidth, btnHeight;
+	private int menuBtnX, menuBtnY;
+	private int groupWidth, groupHeight, groupX, groupY;
+	// Use this for initialization
+	void Start () {
+		btnWidth = 80;
+		btnHeight = 30;
+		groupWidth = 100;
+		groupHeight = 300;
+		groupX = Screen.width - groupWidth;
+		groupY = Screen.height/2 - groupHeight/2;
+		menuBtnX = 10;
+		menuBtnY = 40;
+	}
+	void OnGUI ()
+	{
+		// Make a group on the center of the screen
+		GUI.BeginGroup (new Rect (groupX, groupY, groupWidth, groupHeight));
+		// All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
+
+
+		GUI.Box (new Rect (0, 0, groupWidth, groupHeight), "Move log");
+		if (GUI.Button (new Rect (menuBtnX, menuBtnY, btnWidth, btnHeight), "Main menu")) {
+				Application.LoadLevel (0);
+		}
+
+		Vector2 scrollPosition = Vector2.zero;
+
+		Rect scrollViewDisplayPositionAndSize = new Rect (0, menuBtnY + btnHeight + 10, 100, 200);
+		Rect scrollViewInteriorPositionAndSize = new Rect (0, 0, 200, 200);
+		GUI.BeginScrollView(scrollViewDisplayPositionAndSize, scrollPosition, scrollViewInteriorPositionAndSize);
+		GUI.Button(new Rect(0, 0, 100, 20), "Top");
+		GUI.Button(new Rect(120, 180, 100, 20), "Bottom-right");
+		GUI.EndScrollView();
+
+		GUI.EndGroup ();
+	}
+}
