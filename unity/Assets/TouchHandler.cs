d58991e77e86fd73cd668b1c8822c0eab2828edf
Steven Janovitz <stevenjanovitz@wustl.edu>
2014-07-20 14:25:22 -0400

    Center on object when clicked, beginning of zoom

--- unity/Assets/TouchHandler.cs
+++ unity/Assets/TouchHandler.cs
@@ -7,6 +7,7 @@ public class TouchHandler : MonoBehaviour {
 	Detective d;
 	bool alternateTouch; //prevents mouse from registering a tocuh twice
 	public GameLogic gameLogic;
+	public CameraMovement cameraMovement;
 
 	// Use this for initialization
 	void Start () {
@@ -18,16 +19,16 @@ public class TouchHandler : MonoBehaviour {
 			detectives.Add(gameObject.GetComponent<Detective>());
 		}
 		bool alternate = false;
+
+		gameLogic = GetComponent<GameLogic> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) 
-		//if (Input.GetMouseButtonDown(0))
 		{
 			alternateTouch = !alternateTouch;
 			Vector3 inputPosition  = Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x,Input.GetTouch(0).position.y, 0));
-			//Vector3 inputPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			RaycastHit2D hit = Physics2D.Raycast(inputPosition, Vector2.zero);
 
 			if (hit.collider != null) {
@@ -36,6 +37,9 @@ public class TouchHandler : MonoBehaviour {
 					Detective d = hit.collider.gameObject.GetComponent<Detective>();
 					d.Select();
 				}
+
+				Vector3 center = hit.collider.bounds.center;
+				Camera.main.transform.position = new Vector3(center.x, center.y, Camera.main.transform.position.z);
 			}
 
 			foreach(Detective d in detectives){
@@ -46,4 +50,6 @@ public class TouchHandler : MonoBehaviour {
 			}
 		}
 	}
+	
+
 }
