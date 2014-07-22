db994816d1462482b0557dbcf4c7492e13cb7283
lopezl27 <luis27lopez21@gmail.com>
2014-07-21 21:06:32 -0500

    added popup class

---
+++ unity/Assets/PopUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public Rect windowRect = new Rect(20, 20, 120, 50);
+	void OnGUI() {
+		windowRect = GUI.Window(0, windowRect, DoMyWindow, "My Window");
+	}
+	void DoMyWindow(int windowID) {
+		if (GUI.Button(new Rect(10, 20, 100, 20), "Hello World"))
+			print("Got a click");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
